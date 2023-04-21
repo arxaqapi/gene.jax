517c92bcd8aed3617f26d69002afa824eb3166b2
arxaqapi <arxaqapi@protonmail.com>
2023-04-21 13:26:20 +0200

    fix: rollout_problem transfered to lax

--- gene/evaluate.py
+++ gene/evaluate.py
@@ -6,6 +6,8 @@ import flax.linen as nn
 
 from gene.network import LinearModel
 
+import logging
+from functools import partial
 
 def _L2_dist(x, base, target_offset, d: int):
     diff = lax.dynamic_slice(x, (base,), (d,)) - lax.dynamic_slice(
@@ -45,17 +47,14 @@ def _genome_to_model(genome: list[float], settings: dict):
             "kernel": weight_matrix,
             "bias": jnp.zeros((layer_out,)),
         }
-        # print(model_parameters[f"Dense_{i}"]['kernel'].shape)
 
     # To parameter FrozenDict
     model = LinearModel(layer_dimensions[1:])
     model_parameters = nn.FrozenDict({"params": model_parameters})
-    # print(jax.tree_util.tree_map(lambda x: x.shape, model_parameters))
-    # exit(10)
     return model, model_parameters
 
 
-def _rollout_problem(
+def _rollout_problem_lax(
     model: nn.Module, model_parameters: dict, rng: jrd.KeyArray, settings: dict
 ):
     rng, rng_reset = jrd.split(rng, 2)
@@ -69,11 +68,11 @@ def _rollout_problem(
         rng, rng_step = jrd.split(rng, 2)
         action = jnp.argmax(model.apply(model_parameters, obs))
         n_obs, n_state, reward, done, _ = env.step(rng_step, state, action, env_params)
-
+        
         new_val = n_obs, n_state, done, rng, cum_reward + reward
         return new_val
 
-    val = lax.while_loop(lambda val: val[2], rollout_loop, (obs, state, False, rng, 0))
+    val = lax.while_loop(lambda val: jnp.logical_not(val[2]), rollout_loop, (obs, state, False, rng, 0))
     _, _, _, _, cum_reward = val
 
     return cum_reward
@@ -87,10 +86,14 @@ def evaluate_individual(
     # Genome to model
     model, model_parameters = _genome_to_model(genome, settings=settings)
     # run_rollout
-    fitness = _rollout_problem(
+    fitness = _rollout_problem_lax(
         model=model,
         model_parameters=model_parameters,
         rng=rng,
         settings=settings,
     )
+
+    logger = logging.getLogger('logger')
+    logger.info(f'{fitness=}')
+
     return fitness
