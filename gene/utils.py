6c5cfdcbb677b86ff8707ee3ef9fd20de7aeb7eb
arxaqapi <arxaqapi@protonmail.com>
2023-04-21 16:35:08 +0200

    feat: genome_size method

---
+++ gene/utils.py
@@ -0,0 +1,11 @@
+
+
+
+def genome_size(settings: dict):
+    """Computes the effective size of the genome based on the layers dimensionnalities.
+    """
+    # The first value in layer_dimension does is only used for the dimensionnality
+    # of the input features. So biases are attributed to it
+    d = settings["d"]
+    l_dims = settings["net"]["layer_dimensions"]
+    return l_dims[0] * d + sum(l_dims[1:]) * (d + 1)
