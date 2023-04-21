a56b29a569979925669a9f082417b7f34fbdfad3
arxaqapi <arxaqapi@protonmail.com>
2023-04-21 17:00:03 +0200

    [bip bop]: backup (2023.04.21)

--- gene/evaluate.py
+++ gene/evaluate.py
@@ -1,10 +1,13 @@
 import jax.random as jrd
 import jax.numpy as jnp
 from jax import jit, vmap, lax
+import jax
 import gymnax
 import flax.linen as nn
 
 from gene.network import LinearModel
+from gene.utils import genome_size
+
 
 
 def _L2_dist(x, base, target_offset, d: int):
@@ -21,13 +24,14 @@ _jitted_L2_dist = jit(_vvmap_L2_dist, static_argnames=["d"])
 
 def _genome_to_model(genome: list[float], settings: dict):
     layer_dimensions = settings["net"]["layer_dimensions"]
-    assert genome.shape[0] == sum(layer_dimensions)
+    assert genome.shape[0] == genome_size(settings)
 
     # FIXME: Testing without biases for the moment
     model_parameters = {}
     for i, (layer_in, layer_out) in enumerate(
         zip(layer_dimensions[:-1], layer_dimensions[1:])
     ):
+        # ==== Weights / Kernel
         position_offset = sum(layer_dimensions[:i])
         # indexes of the previous layer neurons
         src_idx = position_offset + jnp.arange(
@@ -41,14 +45,33 @@ def _genome_to_model(genome: list[float], settings: dict):
         )
 
         weight_matrix = _jitted_L2_dist(genome, src_idx, target_idx, settings["d"])
+
+        offset = sum(layer_dimensions[1 : 1 + i])
+        nD = sum(layer_dimensions) * settings["d"]
+        start_i = nD + offset  # _end_i = start_i + layer_out
+        
+        # Biases are directly encoded into the genome
         model_parameters[f"Dense_{i}"] = {
             "kernel": weight_matrix,
             "bias": jnp.zeros((layer_out,)),
+            # "bias": lax.dynamic_slice(genome, (start_i,), (layer_out,)),
         }
-    # return {"params": model_parameters}
+    # ==== Biases
+    # for i, layer_out in enumerate(layer_dimensions[1:]):
+    #     offset = sum(layer_dimensions[1 : 1 + i])
+    #     nD = sum(layer_dimensions) * settings["d"]
+    #     # nD + offset
+    #     start_i = nD + offset
+    #     # nD + layer_out + offset
+    #     _end_i = start_i + layer_out
+    #     # slice of size genome[start_i: start_i + layer_out]
+    #     # Biases are directly encoded into the genome
+    #     model_parameters[f"Dense_{i}"]["bias"] = lax.dynamic_slice(genome, (start_i,), (layer_out,))
+
     # To parameter FrozenDict
     model = LinearModel(layer_dimensions[1:])
     model_parameters = nn.FrozenDict({"params": model_parameters})
+    # print(jax.tree_util.tree_map(lambda x: x.shape, model_parameters))
     return model, model_parameters
 
 
@@ -83,6 +106,34 @@ def _rollout_problem_lax(
     _, _, _, _, cum_reward = val
 
     return cum_reward
+
+
+# def _rollout_problem(
+#     model: nn.Module, model_parameters: dict, rng: jrd.KeyArray, settings: dict
+# ):
+#     rng, rng_reset = jrd.split(rng, 2)
+
+#     env, env_params = gymnax.make(settings["problem"]["environnment"])
+#     obs, state = env.reset(rng_reset, env_params)
+
+#     cum_reward = 0
+#     done = False
+#     while not done:
+#         # Update RNG
+#         rng, rng_step = jrd.split(rng, 2)
+#         # Sample a random action.
+#         action_disrtibution = model.apply(model_parameters, obs)
+#         action = jnp.argmax(action_disrtibution)
+
+#         # Perform the step transition.
+#         n_obs, n_state, reward, done, _ = env.step(rng_step, state, action, env_params)
+#         # Update Stats
+#         cum_reward += reward
+
+#         obs = n_obs
+#         state = n_state
+
+#     return cum_reward
 
 
 def evaluate_individual(
--- run.py
+++ run.py
@@ -10,6 +10,7 @@ import evosax
 from tqdm import tqdm
 
 from gene.evaluate import evaluate_individual
+from gene.utils import genome_size, genome_size_naive
 
 
 def run(
@@ -22,26 +23,30 @@ def run(
     # FIXME: no bias for the moment: add with n(d + 1): sum() * (settings['d'] + 1)
     strategy = evosax.Strategies[settings["evo"]["strategy_name"]](
         popsize=settings["evo"]["population_size"],
-        num_dims=sum(settings["net"]["layer_dimensions"]),
+        num_dims=genome_size(settings)  #  sum(settings["net"]["layer_dimensions"]),
     )
+
+    print(genome_size(settings), genome_size_naive(settings))
+    exit(234)
     fit_shaper = evosax.FitnessShaper(maximize=settings["problem"]["maximize"])
-    es_params = strategy.default_params.replace(init_min=-2, init_max=2)
+    es_params = strategy.default_params.replace(init_min=-10, init_max=10)
     state = strategy.initialize(rng_init, es_params)
 
     # FIXME: uses the same key for each generation
-    jit_vmap_evaluate_individual = jit(
-        vmap(partial(evaluate_individual, settings=settings))
-    )
+    vmap_evaluate_individual = vmap(partial(evaluate_individual, settings=settings))
+    jit_vmap_evaluate_individual = jit(vmap_evaluate_individual)
 
-    for _generation in tqdm(
-        range(settings["evo"]["n_generations"]), desc="Generation loop", position=0
-    ):
+    # for _generation in tqdm(
+    for _generation in range(settings["evo"]["n_generations"]):
         # RNG key creation for downstream usage
         rng, rng_gen, rng_eval = jrd.split(rng, 3)
+        # Here, each individual has an unique random key used for evaluation purposes
         rng_eval_v = jrd.split(rng_eval, settings["evo"]["population_size"])
         # NOTE - Ask
         x, state = strategy.ask(rng_gen, state, es_params)
         # NOTE - Evaluate
+        # temp_fitness = vmap_evaluate_individual(x, rng_eval_v)
+        # temp_fitness = jnp.array([evaluate_individual(genome, rng_eval_v[0], settings) for genome in x])
         temp_fitness = jit_vmap_evaluate_individual(x, rng_eval_v)
         fitness = fit_shaper.apply(x, temp_fitness)
 
@@ -78,11 +83,11 @@ if __name__ == "__main__":
         "d": 1,
         "evo": {
             "strategy_name": "OpenES",
-            "n_generations": 10,
+            "n_generations": 25,
             "population_size": 20,
         },
         "net": {
-            "layer_dimensions": [4, 32, 2],  # Infer network config from dimensions
+            "layer_dimensions": [4, 32, 2],  # 72 = (4 + 32 + 2)+ 32 + 2
         },
         "problem": {
             "environnment": "CartPole-v1",
