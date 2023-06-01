2a67b7edaf149ae2b4745f56bb6ae6dcb81e238d
arxaqapi <arxaqapi@protonmail.com>
2023-06-01 15:09:11 +0200

    feat: added brax vis func

--- gene/visualization.py
+++ gene/visualization.py
@@ -1,12 +1,14 @@
 import jax.numpy as jnp
 import jax.random as jrd
+from jax.tree_util import tree_map
+from jax import jit, lax
 from gymnax.visualize import Visualizer
 import gymnax
 
 import time
 from pathlib import Path
 
-from gene.evaluate import genome_to_model
+from gene.evaluate import genome_to_model, get_brax_env
 
 
 def run_env(genome: jnp.ndarray, config: dict, rng: jrd.KeyArray = jrd.PRNGKey(0)):
@@ -43,3 +45,37 @@ def run_env(genome: jnp.ndarray, config: dict, rng: jrd.KeyArray = jrd.PRNGKey(0
     vis.animate(str(loc), view=False)
 
     return loc
+
+
+def visualize_brax(config: dict, genome, rng: jrd.KeyArray = jrd.PRNGKey(0)):
+    # 1. genome_to_model
+    model, model_parameters = genome_to_model(genome, config)
+
+    # 2. get env
+    env = get_brax_env(config)
+
+    # 3. rollout w. static evaluation function
+    base_state = jit(env.reset)(rng)
+
+    def f(carry, x: None):
+        cur_state = carry
+
+        actions = model.apply(model_parameters, cur_state.obs)
+        new_state = jit(env.step)(cur_state, actions)
+
+        new_carry = new_state
+        return new_carry, cur_state.pipeline_state
+
+    _carry, pipeline_states = lax.scan(
+        f=f,
+        init=base_state,
+        xs=None,
+        length=config["problem"]["episode_length"],
+    )
+
+    flat_pipeline_states = [
+        tree_map(lambda x: x[i], pipeline_states)
+        for i in range(config["problem"]["episode_length"])
+    ]
+
+    return env, flat_pipeline_states
