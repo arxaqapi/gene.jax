f5b405c63cf2c2927902e2f3ae92b322f549f41a
arxaqapi <arxaqapi@protonmail.com>
2023-06-07 22:36:12 +0200

    chore cleanup

--- gene/utils.py
+++ gene/utils.py
@@ -1,11 +1,7 @@
 from functools import partial
 
-import jax.random as jrd
-import jax.numpy as jnp
-from jax import vmap, jit
+from jax import jit
 import matplotlib.pyplot as plt
-
-from gene.evaluate import evaluate_individual
 
 
 def plot(*args, info: str = ""):
@@ -22,32 +18,6 @@ def plot(*args, info: str = ""):
     plt.title(f"Mean fitness over generations\n{info}")
     plt.legend()
     plt.show()
-
-
-def evaluate_mean_population(x, config: dict, rng: jrd.KeyArray = jrd.PRNGKey(0)):
-    """x: (batch, d)"""
-    # Retrieve the theoretical individual at the center of the population
-    center = jnp.mean(x, axis=0)
-    # Evaluate the individual
-    fitness = evaluate_individual(center, rng, config=config)
-    return fitness
-
-
-def evaluate_bench(
-    run_f, config: dict, rng: jrd.KeyArray = jrd.PRNGKey(0), n: int = 10
-):
-    rng_run = jrd.split(rng, n)
-    vmap_run = vmap(partial(run_f, config=config))
-    _, logs, center_fitness = vmap_run(rng=rng_run)
-    return {
-        "fit_best": logs["log_gen_1"].mean(axis=0),
-        "fit_best_std": logs["log_gen_1"].std(axis=0),
-        "mean_fit": logs["log_gen_mean"].mean(axis=0),
-        "mean_fit_std": logs["log_gen_std"].mean(axis=0),
-        "logs": logs,
-        "population_center": center_fitness.mean(axis=0),
-        "population_std": center_fitness.std(axis=0),
-    }
 
 
 def parjit(static: tuple[int] = None):
