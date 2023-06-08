1f1237bb42c1f3e093f4008d73e3d2b4dbbc727f
arxaqapi <arxaqapi@protonmail.com>
2023-06-08 10:22:02 +0200

    fix: tests

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,6 +1,7 @@
 import unittest
 import chex
 import jax.numpy as jnp
+import jax.random as jrd
 
 from gene.tracker import Tracker
 
@@ -11,20 +12,36 @@ class TestTrackerObject(unittest.TestCase):
             "evo": {
                 "n_generations": 30,
             },
+            "net": {"layer_dimensions": [17, 128, 128, 6]},
+            "encoding": {"type": "direct"},
         }
 
     def test_training_top_k_fit(self):
         tracker = Tracker(self.config, 3)
         t_state = tracker.init()
 
-        t_state = tracker.update(t_state, None, jnp.array([1, 2.0, 6.0, 8.0, 2.0, 3.0]))
+        t_state = tracker.update(
+            tracker_state=t_state,
+            fitnesses=jnp.array([1, 2.0, 6.0, 8.0, 2.0, 3.0]),
+            mean_ind=None,
+            eval_f=(lambda *_: None),
+            rng_eval=jrd.PRNGKey(0),
+        )
+
         self.assertIsNone(
             chex.assert_trees_all_close(
                 jnp.array([8.0, 6.0, 3.0]), t_state["training"]["top_k_fit"][0]
             )
         )
 
-        t_state = tracker.update(t_state, None, jnp.array([13.0, -6.0, 4.0]))
+        t_state = tracker.update(
+            tracker_state=t_state,
+            fitnesses=jnp.array([13.0, -6.0, 4.0]),
+            mean_ind=None,
+            eval_f=(lambda *_: None),
+            rng_eval=jrd.PRNGKey(0),
+        )
+
         self.assertIsNone(
             chex.assert_trees_all_close(
                 jnp.array([13.0, 8.0, 6.0]),
@@ -36,14 +53,27 @@ class TestTrackerObject(unittest.TestCase):
         tracker = Tracker(self.config, 3)
         t_state = tracker.init()
 
-        t_state = tracker.update(t_state, None, jnp.array([0.0, 2.0, 4.0]))
+        t_state = tracker.update(
+            tracker_state=t_state,
+            fitnesses=jnp.array([0.0, 2.0, 4.0]),
+            mean_ind=None,
+            eval_f=(lambda *_: None),
+            rng_eval=jrd.PRNGKey(0),
+        )
 
         self.assertIsNone(
             chex.assert_trees_all_close(
                 2.0, t_state["training"]["empirical_mean_fit"][0]
             )
         )
-        t_state = tracker.update(t_state, None, jnp.array([10.0, 22.0, -23.0]))
+        t_state = tracker.update(
+            tracker_state=t_state,
+            fitnesses=jnp.array([10.0, 22.0, -23.0]),
+            mean_ind=None,
+            eval_f=(lambda *_: None),
+            rng_eval=jrd.PRNGKey(0),
+        )
+
         self.assertIsNone(
             chex.assert_trees_all_close(
                 2.0, t_state["training"]["empirical_mean_fit"][0]
