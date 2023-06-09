f92b43296eb6c34d7073c0546f68c7490728e185
arxaqapi <arxaqapi@protonmail.com>
2023-06-09 20:41:12 +0200

    feat: run file

--- run_distance_f.py
+++ run_distance_f.py
@@ -1,13 +1,20 @@
+import wandb
+
 from gene.learn_distance import learn_distance_f_evo
 
 
 if __name__ == "__main__":
     import json
 
-    with open("config/brax.json") as f:
+    with open("config/distance_learning.json") as f:
         config = json.load(f)
 
-    fit, center = learn_distance_f_evo(config)
+    wdb_run = wandb.init(
+        project="Distance-tests",
+        config=config,
+    )
+
+    fit, center = learn_distance_f_evo(config, wdb_run)
 
     print(sorted(fit, reverse=True)[:3])
     print(f"{center=}")
