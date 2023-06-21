4c87d4e09787a7677f18114cd79a0d1ef5e4d578
arxaqapi <arxaqapi@protonmail.com>
2023-06-21 17:53:22 +0200

    feat: tests

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from gene.core.distances import pL2Distance, _a
+from gene.core.decoding import GENEDecoder
 
 
 class TestAFunc(unittest.TestCase):
@@ -44,4 +45,38 @@ class TestpL2(unittest.TestCase):
         )
         self.assertEqual(
             self.df.measure([-x_1, -x_2], 0, 1), np.linalg.norm(-x_1 - (-x_2), 2)
+        )
+
+
+
+class TestpL2InDecoder(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config = {}
+        self.decoder = GENEDecoder(self.config, pL2Distance())
+
+    def test_pL2_dist_pos(self):
+        x_1 = np.array([0.9, 1.2, -0.5, 4.0])
+        x_2 = np.array([-1.0, 3.2, 0.4, -0.8])
+
+        self.assertEqual(
+            self.decoder.distance_function.measure([x_1, x_2], 0, 1), 1 * np.linalg.norm(x_1 - x_2, 2)
+        )
+
+    def test_pL2_dist_neg(self):
+        x_1 = np.array([0.9, 1.2, -0.5, -4.0])
+        x_2 = np.array([-1.0, 3.2, 0.4, -0.8])
+
+        self.assertEqual(
+            self.decoder.distance_function.measure([x_1, x_2], 0, 1), -1 * np.linalg.norm(x_1 - x_2, 2)
+        )
+
+    def test_pL2_extra(self):
+        x_1 = np.array([0.9, 1.2, -0.5, 4.0])
+        x_2 = np.array([-1.0, 3.2, 0.4, -0.8])
+
+        self.assertEqual(
+            self.decoder.distance_function.measure([x_1, x_2], 0, 1), np.linalg.norm(x_1 - x_2, 2)
+        )
+        self.assertEqual(
+            self.decoder.distance_function.measure([-x_1, -x_2], 0, 1), np.linalg.norm(-x_1 - (-x_2), 2)
         )
