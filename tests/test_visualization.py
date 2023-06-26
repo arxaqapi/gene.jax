0f9fe53a1a873e52cb43351d6e3007464ce5e417
arxaqapi <arxaqapi@protonmail.com>
2023-06-26 17:06:48 +0200

    feat: temp test viz

---
+++ tests/test_visualization.py
@@ -0,0 +1,40 @@
+import unittest
+
+import jax.numpy as jnp
+
+from gene.visualize.visualize_brax import visualize_brax, render_brax
+from gene.core.models import BoundedLinearModelConf
+from gene.core.distances import Distance_functions
+from gene.core.decoding import GENEDecoder
+
+
+class TestVizBrax(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config = {
+            "encoding": {"d": 3, "distance": "pL2", "type": "gene"},
+            "net": {"layer_dimensions": [18, 128, 128, 6]},
+            "task": {"environnment": "halfcheetah", "episode_length": 1000},
+        }
+        self.model = BoundedLinearModelConf(self.config)
+        self.df = Distance_functions[self.config["encoding"]["distance"]]()
+        self.genome = jnp.zeros((GENEDecoder(self.config, self.df).encoding_size(),))
+
+    def test_output_file_v1(self):
+        # FIXME - AttributeError: 'System' object has no attribute 'replace'
+        #  File "/home/gene.jax/gene/visualize/visualize_brax.py", line 74, in render_brax
+        self.assertIsNone(
+            render_brax(
+                *visualize_brax(self.config, self.genome, self.model, self.df),
+                output_file="/tmp/test_file",
+            )
+        )
+
+    # def test_output_file_v2(self):
+    #     self.assertIsNone(
+    #         render_brax(
+    #             *visualize_brax(
+    #                 self.config, self.genome, self.model, self.df, use_v1=False
+    #             ),
+    #             output_file="/tmp/test_file_2",
+    #         )
+    #     )
