6bc4d7b1488c88737f640a4b27c9acb0794cdc45
arxaqapi <arxaqapi@protonmail.com>
2023-07-10 18:13:14 +0200

    feat: added maeta skeleton

---
+++ gene/meta.py
@@ -0,0 +1,22 @@
+# 
+def meta_learn_nn(config: dict):
+    """Meta evolution of a neural network parametrized distance function
+    
+    - Direct encoding for meta-genotypes
+    -
+
+    Instantiate a set of parametrized dfs
+    for each df
+        evaluate on a curriculum of tasks, see 2023_07_07.md
+    use the fitness values to inform the df genome update
+
+    return a learned, parametrized distance function
+    """
+
+    pass
+
+
+def meta_learn_cgp(config: dict):
+    """Meta evolution of a cgp parametrized distance function
+    """
+    pass
