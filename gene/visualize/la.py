72d0894d9eda7c3fe03ae1fea85f392295fa0e07
arxaqapi <arxaqapi@protonmail.com>
2023-07-11 16:17:05 +0200

    feat: fixed LA

--- gene/visualize/la.py
+++ gene/visualize/la.py
@@ -5,30 +5,42 @@ Perform [Fitness|Loss] Landscape Analysis on a specific run.
 from functools import partial
 from pathlib import Path
 
-import jax.numpy as jnp
 import jax.random as jrd
 from jax import jit, vmap, Array
 
 from gene.v1.lla import interpolate_2D, plot_ll
 from gene.core.evaluation import get_braxv1_env
+from gene.learning import brax_eval
+from gene.core.decoding import get_decoder
 
 
-def run_lla_brax(
+def run_fla_brax(
     config: dict,
     plot_title: str,
     initial_genome: Array,
     final_genome: Array,
     wandb_run,  # only used to upload files to wandb
     n: int = 100,
+    rng: jrd.KeyArray = jrd.PRNGKey(0),
 ) -> None:
+    """Runs 2D Fitness Landscape Analysis between an `initial_genome`
+    and a `final_genome`. Saves the plot on weights and biases
+
+
+    Args:
+        config (dict): config file of the run
+        plot_title (str): title given to the output plot
+        initial_genome (Array): Initial genome used to interpolate the landscape
+        final_genome (Array): Final genome used to interpolate the landscape
+        wandb_run (_type_): Run object used to upload the plots.
+    """
     rng, interpolation_rng, eval_rng = jrd.split(rng, 3)
 
-    # NOTE - 1. Get env
-    # FIXME - pass it down ?
+    # NOTE - 1. Get env & decoder
     env = get_braxv1_env(config)
+    decoder = get_decoder(config)
 
     # NOTE - 2. get genomes
-    # FIXME - (as parameters)
     initial_genome, final_genome = initial_genome, final_genome
 
     # NOTE - 3. interpolate
@@ -37,27 +49,26 @@ def run_lla_brax(
     )
 
     # NOTE - 4. evaluate at each interpolation step
-    # FIXME - eval_func to fix, or to pass down?
-    part_eval = partial(evaluate_individual_brax, config=config, rng=eval_rng, env=env)
+    part_eval = partial(
+        brax_eval, rng=eval_rng, decoder=decoder, config=config, env=env
+    )
     vmap_eval = jit(vmap(part_eval, in_axes=(0)))
 
     values = vmap_eval(genomes)
 
     # NOTE - 5. plot landscape
-    # FIXME - fix directory
-    plot_save_path = Path(run.dir) / "lla" / title
+    plot_save_path = Path(wandb_run.dir) / "lla" / plot_title
     plot_save_path.parent.mkdir(parents=True, exist_ok=True)
 
     plot_ll(
         values,
         xs,
         ys,
-        evaluate_individual_brax(initial_genome, eval_rng, config, env=env),
-        evaluate_individual_brax(final_genome, eval_rng, config, env=env),
+        brax_eval(initial_genome, eval_rng, decoder, config, env=env),
+        brax_eval(final_genome, eval_rng, decoder, config, env=env),
         plot_title=str(plot_save_path),
     )
 
     # NOTE - 6. Save LL to run
-    # TODO - test me
     wandb_run.upload_file(f"{str(plot_save_path)}.html", root=f"{wandb_run.dir}")
     wandb_run.upload_file(f"{str(plot_save_path)}.png", root=f"{wandb_run.dir}")
