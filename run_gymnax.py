6ae5b6355166b3b8666f1d396d9c2b60a8af9c8e
arxaqapi <arxaqapi@protonmail.com>
2023-07-27 19:21:59 +0200

    feat: argparse

--- run_gymnax.py
+++ run_gymnax.py
@@ -1,3 +1,5 @@
+import argparse
+
 import wandb
 import jax.random as jrd
 
@@ -9,11 +11,38 @@ from gene.core.distances import get_df
 if __name__ == "__main__":
     fail_if_not_device()
 
-    config = load_config("config/acrobot.json")
+    parser = argparse.ArgumentParser(
+        prog="Gymnax running script",
+        description="Performs policy search using an evolutionnary strategy.",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--env",
+        type=str,
+        default="cartpole",
+        choices=["cartpole", "acrobot", "pendulum"],
+        help="Name of the environnment",
+    )
+    parser.add_argument(
+        "-p",
+        "--project",
+        type=str,
+        default="Gymnax benchmarks",
+        help="Name of the wandb project",
+    )
+
+    args = parser.parse_args()
+
+    config = load_config(f"config/{args.env}.json")
     validate_json(config)
 
     df = get_df(config)()
 
-    wdb_run = wandb.init(project="Acrobot", config=config)
+    wdb_run = wandb.init(
+        project=args.project,
+        config=config,
+        name=f"{args.env}-{config['encoding']['distance']}",
+    )
 
     learn_gymnax_task(df, jrd.PRNGKey(0), config, wandb)
